3df67eefb95178c4c124a603c2bdea8d406b469e
Daniel Donenfeld <dbd64@users.noreply.github.com>
2014-12-15 03:22:13 -0500

    Initial Commit

---
+++ JSONReader/JSONReader.h
@@ -0,0 +1,19 @@
+//
+//  JSONReader.h
+//  JSONReader
+//
+//  Created by Daniel Donenfeld on 12/15/14.
+//  Copyright (c) 2014 Daniel Donenfeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONReader.
+FOUNDATION_EXPORT double JSONReaderVersionNumber;
+
+//! Project version string for JSONReader.
+FOUNDATION_EXPORT const unsigned char JSONReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONReader/PublicHeader.h>
+
+
